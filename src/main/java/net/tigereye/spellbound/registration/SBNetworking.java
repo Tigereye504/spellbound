53549e6ce24dda2d0078453da37c3b60f2443674
tigereye504 <dvdtroyer351@gmail.com>
2022-08-19 14:39:16 -0500

    No more phase whiplash Personality fix

--- src/main/java/net/tigereye/spellbound/registration/SBNetworking.java
+++ src/main/java/net/tigereye/spellbound/registration/SBNetworking.java
@@ -1,8 +1,12 @@
 package net.tigereye.spellbound.registration;
 
 import net.fabricmc.fabric.api.networking.v1.ServerPlayNetworking;
+import net.minecraft.network.packet.s2c.play.PlayerPositionLookS2CPacket;
 import net.minecraft.util.Identifier;
 import net.tigereye.spellbound.Spellbound;
+
+import java.util.HashSet;
+import java.util.Set;
 
 public class SBNetworking {
     public static final Identifier TELEPORT_REQUEST_PACKET_ID = new Identifier(Spellbound.MODID,"teleport_request");
@@ -12,7 +16,14 @@ public class SBNetworking {
             double x = buf.readDouble();
             double y = buf.readDouble();
             double z = buf.readDouble();
-            server.execute(() -> client.requestTeleportAndDismount(x,y,z));
+            server.execute(() -> {
+                Set<PlayerPositionLookS2CPacket.Flag> flags = new HashSet<>();
+                flags.add(PlayerPositionLookS2CPacket.Flag.X);
+                flags.add(PlayerPositionLookS2CPacket.Flag.Y);
+                flags.add(PlayerPositionLookS2CPacket.Flag.Z);
+                client.networkHandler.requestTeleport(x,y,z, client.getYaw(), client.getPitch(), flags);
+
+            });
         });
     }
 }
